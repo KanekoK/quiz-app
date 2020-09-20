1d8a5cefe561d4e9f909423cbcef40daf53d2b13
Kaneko Kenya <kaneko@atsu-maru.co.jp>
2020-09-20 14:55:56 +0900

    チェックメソッドの作成

--- src/main/java/com/example/quizapp/QuizAppController.java
+++ src/main/java/com/example/quizapp/QuizAppController.java
@@ -22,4 +22,25 @@ public class QuizAppController {
         Quiz quiz = new Quiz(question, answer);
         quizzes.add(quiz);
     }
+
+    @GetMapping("/check")
+    public String check(@RequestParam String question, @RequestParam boolean answer) {
+        // TODO: 回答が正しいかどうかチェックして、結果を返却する
+        // 指定されたquestionを登録済みのクイズから検索する
+        for (Quiz quiz: quizzes) {
+            // もしクイズが見つかったら
+            if (quiz.getQuestion().equals(question)) {
+                if (quiz.isAnswer() == answer) {
+                    // 登録されているanswerと回答として渡ってきたanswerが一致している場合、「正解」と返却する
+                    return "正解！";
+                } else {
+                    // もし一致していなければ「不正解」と返却する
+                    return "不正解！";
+                }
+            }
+        }
+
+        // クイズがみつからなかった場合は、「問題がありません」と返却する
+        return "問題がありません！";
+    }
 }
